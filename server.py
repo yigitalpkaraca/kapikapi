8cf861b9e4323d6088c04efddad40c79f59f5aec
MeteSLMN <metesalman@posta.mu.edu.tr>
2025-12-17 21:47:07 +0300

    DATABASE VE LOGIN

--- server.py
+++ server.py
@@ -1,6 +1,41 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template,request,session
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+
+
+
+
 
 app = Flask(__name__)
+# --------------------------------------------------------------------
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///veritabani.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key='kapikapi123'
+db = SQLAlchemy(app)
+# --------------------------------------------------------------------
+
+# -----------------------DATABASE DENEME------------------------
+
+class User(db.Model):
+    
+    __tablename__ = 'user' 
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -27,18 +62,53 @@ def courier():
     return render_template('courier.html')
 
 # Giriş Yap (login.html)
-@app.route("/giris")
+@app.route("/giris",methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        girilen_email = request.form.get('email')
+        girilen_sifre = request.form.get('password')
+
+        # Veritabanında bu e-posta ile kayıtlı kullanıcıyı bul 
+        user = User.query.filter_by(email=girilen_email).first()
+
+        if user and user.password == girilen_sifre:
+            # Giriş başarılı! Kullanıcıyı oturuma kaydet
+            session['user_id'] = user.id
+            session['user_name'] = user.fullname
+            return redirect(url_for('home'))
+        else:
+            # Hatalı giriş durumu
+            return "E-posta veya şifre hatalı! Lütfen tekrar dene." 
+    
+    
+    
     return render_template('login.html')
 
 # Kayıt Ol (register.html)
-@app.route("/kayit")
+@app.route("/kayit",methods=['GET','POST'])
 def register():
+    if request.method =='POST':
+        ad = request.form.get('fullname')
+        email = request.form.get('email')
+        sifre = request.form.get('password')
+        # sifre = request.form.get('password')
+        tel = request.form.get('phone')
+
+        yeni_uye = User(fullname=ad,email = email,password=sifre,phone = tel)
+        db.session.add(yeni_uye)
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template('register.html')
 
-if __name__ =="__main__":
-    app.run(debug=True)
+@app.route("/cikis")
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
 
+if __name__ =="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
 
 
 
